a8dea7d3c6cd65935a6c64c7e5f9415525aec32c
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-11-25 17:21:37 +0700

    Improve benarid_web tests

--- apps/benarid_web/lib/benarid_web/endpoint.ex
+++ apps/benarid_web/lib/benarid_web/endpoint.ex
@@ -5,9 +5,9 @@ defmodule BenarIDWeb.Endpoint do
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
-  # plug Plug.Static,
-  #   at: "/", from: :benarid_web, gzip: false,
-  #   only: ~w(css fonts images js favicon.ico robots.txt)
+  plug Plug.Static,
+    at: "/", from: :benarid_web, gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
