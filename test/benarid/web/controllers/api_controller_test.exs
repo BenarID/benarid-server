21d51d8bfd270f456db52a08a8fec9f4cd3ba5ed
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-12-23 09:36:08 +0700

    Add tests for authenticated requests (sans rate)

--- test/benarid/web/controllers/api_controller_test.exs
+++ test/benarid/web/controllers/api_controller_test.exs
@@ -4,23 +4,26 @@ defmodule BenarID.Web.APIControllerTest do
   alias BenarID.Schema.{
     Portal,
     PortalHost,
+    Member,
   }
 
   @portal %{name: "Detik.com", slug: "detikcom", site_url: "http://www.detik.com"}
   @portal_hosts %{hostname: "news.detik.com"}
   @article_url "https://news.detik.com/internasional/3377369/usai-penangkapan-teroris-di-tangsel-australia-imbau-warganya-waspada"
+  @member %{name: "Kairi", email: "kairi@email.com"}
 
   setup %{conn: conn} do
     portal = Repo.insert! Portal.changeset(%Portal{}, @portal)
     _portal_host = Repo.insert! PortalHost.changeset(%PortalHost{}, Map.put(@portal_hosts, :portal_id, portal.id))
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    member = Repo.insert! Member.changeset(%Member{}, @member)
+    {:ok, conn: put_req_header(conn, "accept", "application/json"), data: %{member: member}}
   end
 
   ## /process tests
 
   test "/process: non-existent portal url should return not found", %{conn: conn} do
     conn = post conn, api_path(conn, :process), %{url: "http://foobar.com/baz"}
-    assert json_response(conn, 200)["not_found"] == true
+    assert json_response(conn, 404)["error"] == "not found"
   end
 
   test "/process: matching portal should create new article entry", %{conn: conn} do
@@ -41,10 +44,32 @@ defmodule BenarID.Web.APIControllerTest do
     conn1 = post conn, api_path(conn, :process), %{url: @article_url}
     article_id = json_response(conn1, 200)["id"]
     conn2 = get conn, api_path(conn, :stats), %{id: article_id}
-    assert json_response(conn2, 200)["ok"] == true
+    assert json_response(conn2, 200)["rating"] |> is_list()
+  end
+
+  test "/stats: should have `rated` field if member is authenticated", %{conn: conn, data: data} do
+    conn = authenticate conn, data
+    conn1 = post conn, api_path(conn, :process), %{url: @article_url}
+    article_id = json_response(conn1, 200)["id"]
+    conn2 = get conn, api_path(conn, :stats), %{id: article_id}
+    assert json_response(conn2, 200)["rated"] |> is_boolean()
   end
 
   ## /me tests
 
+  test "/me: should give name of authenticated member", %{conn: conn, data: data} do
+    conn = authenticate conn, data
+    conn = get conn, api_path(conn, :me)
+    response = json_response(conn, 200)
+    assert response["id"] == data.member.id
+    assert response["name"] == data.member.name
+  end
+
   ## /rate tests
+
+  ## helper functions
+
+  defp authenticate(conn, data) do
+    conn |> assign(:user, %{id: data.member.id})
+  end
 end
